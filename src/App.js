f3e599dac4f3c122d1b3dd0eb6e9e87ebd585c4d
Michaela Šimková <michaelasimkova@Michaela-MacBook-Air-2.local>
2021-08-10 12:53:19 +0200

    Added the Github repository url

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "} <a href="https://boring-heyrovsky-f3b4e2.netlify.app/" target="_blank">Michaela Šimková</a> and it is{" "} 
+      <a href="https://github.com/Michaela298/react-weather-app" target="_blank">open-sourced on Github</a>
+      </footer>
     </div>
   );
 }
