b78b53baef1299bcf9600c2eecd708ed12e46a2e
Michaela Šimková <michaelasimkova@Michaela-MacBook-Air-2.local>
2021-08-10 15:04:18 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
+      
+      <Weather />
       <footer>
         This project was coded by{" "} <a href="https://boring-heyrovsky-f3b4e2.netlify.app/" target="_blank" rel="noreferrer">Michaela Šimková</a> and it is{" "} 
       <a href="https://github.com/Michaela298/react-weather-app" target="_blank" rel="noreferrer">open-sourced on Github</a>
