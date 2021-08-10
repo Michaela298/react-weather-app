cd8fb7ad5f198d68fe7371832ba2b83150d6242b
Michaela Šimková <michaelasimkova@Michaela-MacBook-Air-2.local>
2021-08-10 13:45:59 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "} <a href="https://boring-heyrovsky-f3b4e2.netlify.app/" target="_blank" rel="noreferrer">Michaela Šimková</a> and it is{" "} 
       <a href="https://github.com/Michaela298/react-weather-app" target="_blank" rel="noreferrer">open-sourced on Github</a>
       </footer>
+      </div>
     </div>
   );
 }
