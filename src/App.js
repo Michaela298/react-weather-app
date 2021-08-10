d4c3f5f397514f1a94e14f1bbd1fe7a21c067388
Michaela Šimková <michaelasimkova@Michaela-MacBook-Air-2.local>
2021-08-10 13:28:38 +0200

    Added rel="noreferrer" to the links

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project was coded by{" "} <a href="https://boring-heyrovsky-f3b4e2.netlify.app/" target="_blank">Michaela Šimková</a> and it is{" "} 
-      <a href="https://github.com/Michaela298/react-weather-app" target="_blank">open-sourced on Github</a>
+        This project was coded by{" "} <a href="https://boring-heyrovsky-f3b4e2.netlify.app/" target="_blank" rel="noreferrer">Michaela Šimková</a> and it is{" "} 
+      <a href="https://github.com/Michaela298/react-weather-app" target="_blank" rel="noreferrer">open-sourced on Github</a>
       </footer>
     </div>
   );
