f3246a9ea2bb2e5be04092e8d3d8eb6ff6b10b13
Michaela Šimková <michaelasimkova@Michaela-MacBook-Air-2.local>
2021-08-17 13:50:10 +0200

    Imported coordinates to the WeatherForecast.js component

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -2,8 +2,19 @@ import { directive } from "@babel/types";
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+    function handleResponse(response) {
+        console.log(response.data);
+    }
+    console.log(props);
+    const apiKey = "36794cb64ceccf9c27e4232061a9201a";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
     return (
         <div className="WeatherForecast">
             <div className="row">
